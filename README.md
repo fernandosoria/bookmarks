669c8fc3c306d087bc0b8f927923dc411cc55eec
fernandosoria <soria.fernando@gmail.com>
2014-08-22 03:49:39 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Bookmark
+
+### An application that allows users to bookmark URLs via email, peruse other user's bookmarks and maintain a personal index of categorized bookmarks.
+
+fernandosoria-bookmarks
+
+[http://fernandosoria-bookmarks.herokuapp.com/](http://fernandosoria-bookmarks.herokuapp.com/ "Bookmarks App")
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bookmarks::Application.config.secret_key_base = '4f91e7a878d759990ed03cfaf07f69b84166f4a18695cbca669fd9b6399f3b5da15bb1eea83cc3cd21190969b930908969f62eeafda1a967dac4af218b68764c'
